288b1a21235fb6416e710752f93469adc3520454
e3lcool@gmail.com <e3lcool@gmail.com>
2021-06-25 14:25:10 -0700

    Next server w/ ssl

--- app.js
+++ app.js
@@ -1,30 +1,29 @@
 const express = require('express')
 const fs = require('fs')
 const https = require('https')
-const gatsyExpress = require('gatsby-plugin-express');
 const app = express();
 
-const path = require('path');
+const dev = process.env.NODE_ENV !== 'production'
+// const app = next({ dev });
+const app = next(false);
+const handle = app.getRequestHandler();
 
 var credentials = {
 	key: fs.readFileSync('./ssl/privatekey.pem'),
 	cert: fs.readFileSync('./ssl/certificate.pem')
 };
 
-// serve static files before gatsbyExpress
-app.use(express.static('public/'));
-app.use(gatsyExpress('config/gatsby-express.json', {
-  publicDir: 'public/',
-  template: path.resolve('public/404/index.html'),
+app.prepare().then(() => {
+  const server = express()
 
-  // redirects all /path/ to /path
-  // should be used with gatsby-plugin-remove-trailing-slashes
-  redirectSlashes: true,
-}));
+  server.all('*', (req, res) => {
+    return handle(req, res)
+  })
 
-https.createServer(credentials, app).listen(443, '10.3.30.54', () => {
-	console.log('App listening on port 443.')
-});
+  https.createServer(credentials, app).listen(443, '10.3.30.54', () => {
+    console.log('App listening on port 443.')
+  });
+})
 
 // Redirect from http port 80 to https
 var http = require('http');
