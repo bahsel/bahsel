e9d105430fff02c0df3168180273016b3a7fa570
e3lcool@gmail.com <e3lcool@gmail.com>
2021-06-25 16:10:21 -0700

    Faster images

--- components/elements/switchingggb.js
+++ components/elements/switchingggb.js
@@ -1,8 +1,6 @@
 import React, { useState } from "react";
-import Image from 'next/image'
 
-import dayggb from '../../public/images/bgs/dayggb.jpg'
-import nightggb from '../../public/images/bgs/nightggb.png'
+import style from './switchingggb.module.css'
 
 class SwitchingGGB extends React.Component {
     // const day = useState(false);
@@ -13,27 +11,12 @@ class SwitchingGGB extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className={style.wrap}>
                 <div style={{ visibility: this.state.day ? 'visible' : 'hidden' }}>
-                    <Image
-                        src={dayggb}
-                        layout='fill'
-                        placeholder='blurred'
-                        quality={90}
-                        objectPosition='30% 10%'
-                        id='dayggb'
-                        objectFit='cover'
-                        priority={true}></Image>
+                    <img src='/images/bgs/dayggb.jpg' className={style.day}></img>
                 </div>
                 <div style={{ visibility: this.state.day ? 'hidden' : 'visible' }}>
-                    <Image
-                        src={nightggb}
-                        layout='fill'
-                        placeholder='blurred'
-                        quality={90}
-                        objectPosition='60% 10%'
-                        id='nightggb'
-                        objectFit='cover'></Image>
+                    <img src='/images/bgs/nightggb.jpg' className={style.night}></img>
                 </div>
             </div>
         )
--- components/navbar.js
+++ components/navbar.js
@@ -7,31 +7,13 @@ import WidthImage from './templates/width-image'
 
 import * as style from './navbar.module.css'
 
-import banner from '../public/images/banner.png'
-
-import bahselText from '../public/images/bahsel.svg'
-import instagramLogo from '../public/images/socials/instagram.svg'
-import discordLogo from '../public/images/socials/Discord-Logo-Black.svg'
-
 const Navbar = () => {
     return (
         <div className={style.navbar}>
             <Link href="/">
                 <a className={style.homeWrapper}>
-                    <WidthImage className={style.logo}>
-                        <Image src={banner}
-                            alt='BAHSEL Banner'
-                            priority='true'
-                            layout='fill'
-                            objectFit='contain'
-                            quality={10}></Image>
-                    </WidthImage>
-                    <WidthImage className={style.name}>
-                        <Image src={bahselText}
-                            priority='true'
-                            layout='fill'
-                            objectFit='contain'></Image>
-                    </WidthImage>
+                    <img src='/images/banner.png' className={style.logo}></img>
+                    <img src='/images/bahsel.svg' className={style.name}></img>
                 </a>
             </Link>
 
@@ -44,20 +26,10 @@ const Navbar = () => {
             <div className={style.socials}>
                 {/* socials */}
                 <a href="https://www.instagram.com/bayareaesports1" target="_blank" rel="noopener noreferrer">
-                    <HeightImage>
-                        <Image
-                            src={instagramLogo}
-                            layout='fill'
-                            objectFit='contain'></Image>
-                    </HeightImage>
+                    <img src='/images/socials/instagram.svg'></img>
                 </a>
                 <a href="https://discord.gg/Hcuj9cM" target="_blank" rel="noopener noreferrer">
-                    <HeightImage>
-                        <Image
-                            src={discordLogo}
-                            layout='fill'
-                            objectFit='contain'></Image>
-                    </HeightImage>
+                    <img src='/images/socials/Discord-Logo-Black.svg'></img>
                 </a>
             </div>
         </div>
